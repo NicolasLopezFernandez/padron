d4b0d4211aedefe3a0836f3b87f0ebea200fea21
NicolasLopezFernandez <nicolas.lopezfernandez1@usp.ceu.es>
2024-09-30 12:15:51 +0200

    Clases del 30/09/2024 2

---
+++ src/dominio/Habitante.java
@@ -0,0 +1,21 @@
+package dominio;
+public class Habitante{
+        private String nombre;
+	private String apellido1;
+	private String apellido2;
+	public Habitante(){}
+	public Habitante(String nombre_, String apellido1_, String apellido2_){
+		nombre=nombre_;
+		apellido1=apellido1_;
+		apellido2=apellido2_;
+	}
+	public String getNombre(){
+		retun nombre;
+	}
+	public void setNombre(String nombre_){
+		nombre=nombre_;
+	}
+	public String toString(){
+		retun "nombre: "+nombre+" "+apellido1+" "+apellido2;
+	}
+}
